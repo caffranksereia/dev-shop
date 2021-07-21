51f7b02f58852e534ac3cfd30a5b3a93b603d62c
ferafael <circuncisaofabio@icloud.com>
2021-07-21 15:26:52 -0300

    criar as function submit que ao clicar ativa a function searchuser que procura o usuario atras do axio.get e o endereço da api . - criar more detais para ver masi detailhes do usuario com as suas reposições. -renderRepo para rendenizar as reposzies . - getDetail para pegar o repoName para ver as linguagens e os commits.

--- dev-shop/src/listUser.js
+++ dev-shop/src/listUser.js
@@ -1,17 +1,70 @@
 import React, { useState } from 'react';
+import Alluser from './Alluser';
 import axios from 'axios'
 
 function ListUser(props) {
-    const [users,setUsers] = useState('')
-    function searchUsers(){
-        axios.get(`https://api.github.com/users/${users}/repos`).then(response => console.log(response.data))
+    const [users,setUsers] = useState('');
+    const [loading,setLoading] = useState(false);
+    const [repos,setRepos] = useState([]);
+    const [details,setDetails] = useState({});
+    const [detailsLoading,setDetailsLoading] = useState(false);
+    const [openDetails,setOpenDetails] = useState([]);
+
+
+    function submit(e){
+        e.preventDefault();
+        searchUsers();
     }
+    function searchUsers(){
+        setLoading(true);
+        axios.get(`https://api.github.com/users/${users}`)
+        .then(res => {
+            setLoading(false);
+            setRepos(res.data)
+            console.log(res.data)
+        })
+    }
+    function moreDetails() {
+        axios.get(`https://api.github.com/users/${users}/repos`)
+        .then(res =>{
+            setOpenDetails(res.data)
+            console.log(res.data)
+        })
+    }
+    function renderRepo(repo){
+        return(
+            <div className="row"onClick={()=>getDetails(repo.name)} key={repo.id}>
+                <div>{repo.name}</div>
+            </div>
+            
+        )
+    }
+    function getDetails(repoName) {
+        getDetails(true)
+        axios.get(`https://api.github.com/users/${users}/${repoName}`)
+        .then(res =>{
+            setDetailsLoading(false);
+            setDetails(res.data)
+        })
+    }
+    
     return (
         <div className = "listuser">
             <p>{users}</p>
             <input type="text" placeholder= "users" value= {users} onChange = {e => setUsers(e.target.value)}/>
-            <button onClick = {searchUsers}>Pesquisar</button>
+            <button onClick = {submit}>{loading ? "Buscando...":"Buscar"}</button>
+            <div>
+                <h1>User</h1>
+                <div>
+                    <div>
+                        <label className="label">Name:</label>
+                        <span className="value">{repos.login}</span>
+                    </div>
+                    <button onClick= {moreDetails}>Detalhes</button>
+                </div>
+            </div>
         </div>
+    
     );
 }
 
