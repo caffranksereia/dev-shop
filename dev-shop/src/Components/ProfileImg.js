09f287fe1ed636eb576184b4a7f08fbb590d6598
ferafael <circuncisaofabio@icloud.com>
2021-07-23 17:48:53 -0300

    Criado um component para receber a imagaem atravez do props

---
+++ dev-shop/src/Components/ProfileImg.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+
+
+function ProfileImg(props) {
+    const [imgProfile,setImgProfile] = useState([])
+    
+    
+    function showImg(props){
+        
+    }
+
+   
+   
+    return (
+        <div>
+            <h1>Image Profile</h1>
+            <img>
+            
+            </img>
+           
+        </div>
+    
+    );
+}
+
+export default ProfileImg;
