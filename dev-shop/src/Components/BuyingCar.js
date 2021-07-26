bc3d983ac1b407a6dc156d74b29a8ffe2ae3b1ee
ferafael <circuncisaofabio@icloud.com>
2021-07-26 11:14:48 -0300

    Arquivo modificado

---
+++ dev-shop/src/Components/BuyingCar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function BuyingCar(params) {
+    
+
+    return(
+        <div>
+                
+             carrinho
+            
+        </div>
+    );
+
+}
+
+export default BuyingCar;
