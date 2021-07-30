a12dcced7d56dc2587af195975ffadad7dffbd6a
ferafael <circuncisaofabio@icloud.com>
2021-07-30 10:11:36 -0300

    em breve sera apagado

--- dev-shop/src/Components/DetailsUsers.js
+++ dev-shop/src/Components/DetailsUsers.js
@@ -2,9 +2,24 @@ import React from 'react';
 
 
 class DetailsUsers extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            detalhes: '',
+            nome :'',
+            nickname: '',
+            detalhes:'',
+        }
+
+    }
     render(){
         return(
             <div>
+                 <p> Nome:{this.props.nome}</p>
+                 <p> Nick: {this.props.nickname}</p>
+                 <p>localização: {this.props.localização}</p>
+                 <p> Valor:{this.props.valor},00/por horas</p>
                 <p>detalhes:{this.props.detalhes}</p>
             </div>
         );
