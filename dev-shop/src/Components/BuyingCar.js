b20d30d679ec1c5e538fac2c792e893184e4e7f7
ferafael <circuncisaofabio@icloud.com>
2021-07-30 10:08:26 -0300

    alterado os botoes para levar a pagina comprado

--- dev-shop/src/Components/BuyingCar.js
+++ dev-shop/src/Components/BuyingCar.js
@@ -1,36 +1,62 @@
 import React from 'react';
-import axios from 'axios';
-import Comprado from './Comprado';
+import { useHistory } from 'react-router-dom';
+import api from '../Services/api';
 
-class BuyingCar extends React.Component{
-
-    render(){
-        return(
-            <div>
-                <form>
-                    <label>Desenvolvedor:</label>
-                    <p>{this.props.name}</p>
-                    <label>Usuario:</label>
-                    <p>{this.props.nickname}</p>
-                    <label>Bio:</label>
-                    <p>{this.props.Bio}</p>
-                    <label>Valor:</label>
-                    <p>R${this.props.Valor},00/Por horas</p>
+export default function BuyingCar (props){
+    let historia = useHistory();
+    function Details(users){
+        api.get(`https://api.github.com/users/${users}/repos`)
+            .then(res =>{
                     
-                
-                
-                
-                </form>   
-                <div>
-                    <button>Pagar</button>
+                this.setDetalhes(res.data)
+                    console.log(res.data)
+    
+                    
+            }
+       )
+    }
+    
+        function renderDetail(detalhes) {
+            return(
+                <div key ={detalhes.id} >
+                    
+                    <label>nome do projeto: </label>{detalhes.name}
+                    <br></br>
+                    <label>Linguagem: </label>{detalhes.language}
+    
                 </div>
+            )
+        }
+        return(
+            
+            <div>
+                <div>
+                    <h1>Carrinho de compras</h1>
+                </div> 
+               <div>
+               <p> Nome:{props.nome}</p>
+                    <p> Nick: {props.nickname}</p>
+                    <p>Bio:{props.bio}</p>
+                    <p>localização: {props.localização}</p>
+                    <p> Valor:{props.valor},00/por horas</p>
+               </div>
+               <div>
+                   <button>Detalhe</button>
+               </div>
+               <div>
+                    <button onClick ={()=>{
+                        historia.push("/Compras")
+                        }}>
+                            Comprar
+                        
+                    </button>
+               </div>
+
                 
             </div>
         );
-    }
-
+    
   
 
 }
 
-export default BuyingCar;
