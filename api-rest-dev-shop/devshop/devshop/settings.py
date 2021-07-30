f88b3100021acfd98fd50d2862a1a15f15673d87
ferafael <circuncisaofabio@icloud.com>
2021-07-30 10:35:56 -0300

    idk

--- api-rest-dev-shop/devshop/devshop/settings.py
+++ api-rest-dev-shop/devshop/devshop/settings.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-sk= os.getenv('SECRET_KEY')
+sk = os.getenv('SECRET-KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = sk
+SECRET_KEY = 'django-insecure-q7i-92fr_0h!4qquq8g@+9slzlua3yr&())9+oly_l!*nzij#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
