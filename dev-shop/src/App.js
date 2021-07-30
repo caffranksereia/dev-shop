2d82bfd947ea7881b2363efc0ff681cd8e8fe36b
ferafael <circuncisaofabio@icloud.com>
2021-07-30 09:58:54 -0300

    import react-router-dom

--- dev-shop/src/App.js
+++ dev-shop/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
-import SearchProfile from './Components/SearchProfile'
+import './App.css'
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import Comprado from './Components/Comprado'
+import BuyingCar from './Components/BuyingCar';
+import Profile from './Components/Profile';
+import SearchProfile from './Components/SearchProfile';
 
 function App() {
   return (
     <div className="App">
-      <SearchProfile></SearchProfile>
-
+      <Router>
+        <Switch>
+          <Route exact path="/Compras" component={Comprado}/>
+          <Route exact path ="/Carrinho" component={BuyingCar}/>
+          <Route exact path="/Profile" component={Profile}/>
+          <Route exact path="/home" component={SearchProfile}/>
+        </Switch>
+       
+      </Router>
     </div>
   );
 }
