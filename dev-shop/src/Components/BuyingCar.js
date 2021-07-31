8c0b463c97e91ee76ba3a288e5cc208a070248b3
ferafael <circuncisaofabio@icloud.com>
2021-07-30 21:21:34 -0300

    json db

--- dev-shop/src/Components/BuyingCar.js
+++ dev-shop/src/Components/BuyingCar.js
@@ -1,45 +1,24 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import api from '../Services/api';
+import BuyCar from '../Services/BuyCar'
 
-export default function BuyingCar (props){
-    let historia = useHistory();
-    function Details(users){
-        api.get(`https://api.github.com/users/${users}/repos`)
-            .then(res =>{
-                    
-                this.setDetalhes(res.data)
-                    console.log(res.data)
+export default function BuyingCar (){
     
-                    
-            }
-       )
+    
+   const historia = useHistory()
+   
+    function Comprando(){
+        historia.push("/Comprando")
     }
-    
-    function renderDetail(detalhes) {
-        return(
-            <div key ={detalhes.id} >
-                    
-                <label>nome do projeto: </label>{detalhes.name}
-                <br></br>
-                <label>Linguagem: </label>{detalhes.language}
-    
-            </div>
-       )
-    }
+        
         return(
             
             <div>
                 <div>
                     <h1>Carrinho de compras</h1>
                 </div> 
-               <div>
-               <p> Nome:{props.nome}</p>
-                    <p> Nick: {props.nickname}</p>
-                    <p>Bio:{props.bio}</p>
-                    <p>localização: {props.localização}</p>
-                    <p> Valor:{props.valor},00/por horas</p>
-               </div>
+               
+               
                <div>
                    <button>Detalhe</button>
                </div>
@@ -48,9 +27,7 @@ export default function BuyingCar (props){
                    <p></p>
                </div>
                <div>
-                    <button onClick ={()=>{
-                        historia.push("/Compras")
-                        }}>
+                    <button onClick ={Comprando()}>
                             Comprar
                         
                     </button>
@@ -60,6 +37,9 @@ export default function BuyingCar (props){
             </div>
         );
     
+    
+    
+       
   
 
 }
