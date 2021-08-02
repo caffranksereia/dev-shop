f317ebfb0b14a6f57a14b6f27868df7e3269c844
ferafael <circuncisaofabio@icloud.com>
2021-08-02 16:41:39 -0300

    Alterando nomes.

--- dev-shop/src/App.js
+++ dev-shop/src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
-import Comprado from './Components/Comprado'
+import Buying from './Components/Buying'
 import BuyingCar from './Components/BuyingCar';
 import Profile from './Components/Profile';
 import SearchProfile from './Components/SearchProfile';
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/Compras" component={Comprado}/>
+          <Route exact path="/Compras" component={Buying}/>
           <Route exact path ="/Carrinho" component={BuyingCar}/>
           <Route exact path="/Profile" component={Profile}/>
           <Route exact path="/home" component={SearchProfile}/>
