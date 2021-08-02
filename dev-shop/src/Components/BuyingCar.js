3809b468ddf7488803a8956b6023cee19a6c5954
ferafael <circuncisaofabio@icloud.com>
2021-08-02 14:27:51 -0300

    criado as duas variaveis postBuy e setPosby no estado vazio para pegar no banco de dados as informações e colocar na tela usando o useeffect para listar e pelo postBy passar no renderDetail as informções que quero que seja rendenizado quando for chamado dentro do map

--- dev-shop/src/Components/BuyingCar.js
+++ dev-shop/src/Components/BuyingCar.js
@@ -3,6 +3,32 @@ import { useHistory } from 'react-router-dom';
 import BuyCar from '../Services/BuyCar'
 
 export default function BuyingCar (){
+    const [postBuy,setPostBy] = useState([])
+    const historia= useHistory()
+
+    useEffect(()=>{
+        BuyCar.buyCarAll().then(res =>{
+            console.log(res.data)
+            setPostBy(res.data)
+        })
+    },[])
+
+    function renderDetail(postBuy) {
+        return(
+            <div key ={postBuy.id} >
+
+                <label>Nome: </label>{postBuy.name}
+                <br></br>
+                <label>valor: </label>{postBuy.valor}
+                <br></br>
+                <label>Nickname:</label>{postBuy.Username}
+                <br></br>
+                <label>Bio: </label>{postBuy.Bio}
+                <br></br>
+                <label>Detalhes: </label>{postBuy.name}
+                <br></br>
+                <label>Localização: </label>{postBuy.localizacao}
+                <br></br>
     
     
    const historia = useHistory()
