ef181ea97ee40863a260ff05fdd671aed21d1140
ferafael <circuncisaofabio@icloud.com>
2021-08-02 16:41:58 -0300

    arquivo css incluindo.

---
+++ dev-shop/src/Components/Buying.js
@@ -0,0 +1,20 @@
+import  '../CSS/Buying.css'
+
+function Buying() {
+   
+    return(
+        <div className ="body">
+            <div className = "headBuy">
+                <div>
+                    <div>
+                        <h1>Comprado</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+    
+}
+
+export default Buying;
